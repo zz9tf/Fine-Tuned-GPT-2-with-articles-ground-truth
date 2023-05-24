b9771e41c2da72518741c533592f45c0b9b184d0
Zheng <zhengzheng@brandeis.edu>
2023-05-24 15:08:18 -0400

    remote service

---
+++ start.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#SBATCH --job-name=myjob
+#SBATCH --output=myjob.out
+#SBATCH --account=guest
+#SBATCH --partition=guest-gpu
+#SBATCH --qos=low-gpu
+#SBATCH --time=02:00:00
+#SBATCH --gres=gpu:TitanX:1   # Request 1 TitanX GPU
+ 
+# Load modules required for your job
+module load cuda/9.0
+module load share_modules/HOOMD/2.3.5_sp
+ 
+# Path to your Python executable and script
+PYTHON_EXECUTABLE=/path/to/python
+PYTHON_SCRIPT=/path/to/gpt-2-fine-tune.py
+
+# Activate the virtual environment if needed
+# source /path/to/virtualenv/bin/activate
+
+# Run the Python code
+$PYTHON_EXECUTABLE $PYTHON_SCRIPT
