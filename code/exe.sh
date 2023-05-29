a591d2e2ff76aff598d9f0675173bfcb5d020ad5
Zheng <zhengzheng@brandeis.edu>
2023-05-29 18:32:32 -0400

    update

--- code/exe.sh
+++ code/exe.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 #SBATCH --job-name=gpt
 #SBATCH --output=None
-#SBATCH --mail-type=BEGIN, FAIL, END
+#SBATCH --mail-type=ALL
 #SBATCH --mail-user=Zheng_5732021823@outlook.com
+#SBATCH --account=guest
 #SBATCH --partition=guest-gpu
 #SBATCH --qos=low-gpu
-#SBATCH --gres=gpu:TitanX:1   # Request 1 TitanX GPU
 #SBATCH --time=02:00:00
+#SBATCH --gres=gpu:TitanX:1   # Request 1 TitanX GPU
  
 # Load modules required for your job
 module load cuda/9.0
 module load share_modules/HOOMD/2.3.5_sp
 
-python ./code/gpt-2-fine-tune.py > ../log 2>&1
+python gpt-2-fine-tune.py > ../log 2>&1
