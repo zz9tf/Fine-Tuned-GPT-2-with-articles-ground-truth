0377d24f88b837d507bba99078decf1f579e87d4
Zheng <zhengzheng@brandeis.edu>
2024-12-31 10:45:42 -0500

    update reader

--- code/llamaIndex/component/reader/wikipedia_reader.py
+++ code/llamaIndex/component/reader/wikipedia_reader.py
@@ -24,6 +24,82 @@ class WikipediaDumpReader():
         self.worker = worker
         self.pages_per_batch = pages_per_batch
     
+    def _write_to_disk(self, batch_id, batch):
+        cache_file_path = os.path.join(self.cache_dir, f'raw_page_{batch_id}_not_finish.jsonl')
+        with open(cache_file_path, 'w') as cache_file:
+            for page in batch:
+                cache_file.write(json.dumps({'page': page}) + '\n')
+        os.rename(cache_file_path, os.path.join(self.cache_dir, f'raw_page_{batch_id}.jsonl'))
+    
+    def _read_file(self, input_file_path):
+        total_file_size = os.path.getsize(input_file_path)
+        parser = XMLPullParser(events=('start', 'end'))
+        raw_page = None
+        end_page = False
+        page_num = 0
+        batch_id = 0
+        current_batch = []
+        all_batches = []
+        update_len = 0
+        
+        # Load all batches
+        with open(input_file_path, 'r') as input_file:
+            with tqdm(total=total_file_size, desc="Processing", unit="B", unit_scale=True) as pbar:
+                for line in input_file:
+                    parser.feed(line)
+                    for event, element in parser.read_events():
+                        if event == 'start' and 'page' in element.tag:
+                            raw_page = []
+                        if event == 'end' and 'page' in element.tag:
+                            end_page = True
+                    # add line
+                    if isinstance(raw_page, list):
+                        raw_page.append(line)
+                    if end_page:
+                        raw_page = ''.join(raw_page)
+                        current_batch.append(raw_page)
+                        if len(current_batch) > self.pages_per_batch:
+                            all_batches.append([batch_id, current_batch])
+                            batch_id += 1
+                            current_batch = []
+                        page_num += 1
+                        raw_page = None
+                        end_page = False
+                    if update_len > 1e10:
+                        pbar.set_postfix_str(f"page {page_num}")
+                        pbar.update(update_len)
+                        update_len = 0
+                    else:
+                        update_len += len(line)
+                    if len(all_batches) >= 10:
+                        # Write to disk using a ThreadPool for parallel processing of batches
+                        with ThreadPool(self.worker) as pool, tqdm(total=len(all_batches), desc="process batches") as pbar:
+                            # Wrap the pool.imap with tqdm to display a progress bar
+                            for _ in pool.imap(lambda x: self._write_to_disk(x[0], x[1]), all_batches):
+                                pbar.update(1)  # Update the progress bar for each processed batch
+                        all_batches = []
+        # If there are remaining pages in the last batch, add them
+        if current_batch:
+            all_batches.append([batch_id, current_batch])
+
+        if len(all_batches) > 0:
+            # Write to disk using a ThreadPool for parallel processing of batches
+            with ThreadPool(self.worker) as pool, tqdm(total=len(all_batches), desc="process batches") as pbar:
+                # Wrap the pool.imap with tqdm to display a progress bar
+                for _ in pool.imap(lambda x: self._write_to_disk(x[0], x[1]), all_batches):
+                    pbar.update(1)  # Update the progress bar for each processed batch
+        
+    def _parse_files(self):
+        for filename in os.listdir(self.input_dir):
+            if filename.endswith('.xml'):
+                self._read_file(os.path.join(self.input_dir, filename))
+    
+    def _processed_xml(self):
+        for filename in os.listdir(self.cache_dir):
+            if "raw_page" in filename:
+                return True
+        return False
+    
     def _clean_text(self, raw_text):
         cleaned_text = html.unescape(raw_text)
         cleaned_text = re.sub(r'\{\{.*?\}\}', '', raw_text, flags=re.DOTALL)
@@ -47,8 +123,7 @@ class WikipediaDumpReader():
         #     print(abstract.strip())
             print_str += f"Not find abstract at title {title}\n"
             
-        # assert len(abstract) > 0, f"Not find abstract at title {title}"
-            
+        # assert len(abstract) > 0, f"Not find abstract at title {title}"  
         
         return abstract, print_str
 
@@ -114,17 +189,6 @@ class WikipediaDumpReader():
         return {'title': title, 'text': text, 'raw_page': xml_data}
         
     def _process_batch(self, batch_id, page_chunk):
-        # page = self._go_over_string_element(raw_page)
-        # if page:
-        #     for key in page:
-        #         pages_key[key] = pages_key.get(key, 0) + 1
-        #     document, no_abstract = self._read_page(page)
-        #     if document:
-        #         documents.append(document)
-        #         pages_key['no_abstract'] = pages_key.get('no_abstract', 0) + (1 if no_abstract else 0)
-        #     pbar.set_postfix_str(
-        #         f"{pages_key['no_abstract']}/{pages_key['title']}  {(pages_key['no_abstract']/pages_key['title'])*100:.2f}%"
-        #     )
         documents = []
         no_abstract_num = 0
         print_str = f"Start batch id: {batch_id}\n"
@@ -137,129 +201,7 @@ class WikipediaDumpReader():
                 if document:
                     documents.append(document)
                     if no_abstract: no_abstract_num += 1
-        return batch_id, documents, no_abstract_num, print_str
-    
-    # def _read_file(self, input_file_path):
-    #     total_file_size = os.path.getsize(input_file_path)
-    #     parser = XMLPullParser(events=('start', 'end'))
-    #     raw_page = None
-    #     end_page = False
-        
-    #     documents = []
-    #     no_abstract_num = 0
-    #     current_batch = []
-    #     all_batches = []
-        
-    #     with ThreadPool(self.worker) as pool, open(input_file_path, 'r') as input_file:
-    #         with tqdm(total=total_file_size, desc="Processing", unit="B", unit_scale=True) as pbar:
-    #             for line in input_file:
-    #                 parser.feed(line)
-    #                 for event, element in parser.read_events():
-    #                     if event == 'start' and 'page' in element.tag:
-    #                         raw_page = []
-    #                     if event == 'end' and 'page' in element.tag:
-    #                         end_page = True
-    #                 # add line
-    #                 if isinstance(raw_page, list):
-    #                     raw_page.append(line)
-    #                 if end_page:
-    #                     raw_page = ''.join(raw_page)
-    #                     current_batch.append(raw_page)
-    #                     raw_page = None
-    #                     end_page = False
-    #                 if len(current_batch) >= self.pages_per_batch:
-    #                     all_batches.append(current_batch)
-    #                     current_batch = []
-    #                     if len(all_batches) >= self.worker:
-    #                         results = []
-    #                         for batch_id, result in enumerate(pool.imap(
-    #                             lambda batch: self._process_batch(batch_id, batch), all_batches
-    #                         )):
-    #                             results.append(result)
-    #                         for _, worker_documents, worker_no_abstract_num, batch_print_str in results:
-    #                             documents.extend(worker_documents)
-    #                             no_abstract_num += worker_no_abstract_num
-    #                             print(batch_print_str)
-    #                             pbar.set_postfix_str(
-    #                                 f"{no_abstract_num}/{len(documents)}  {100*(no_abstract_num/len(documents)):.2f}%"
-    #                             )
-    #                 pbar.update(len(line))
-                    
-    #     return documents
-    
-    
-    
-        # if self.cache_file is None:
-        #     self.cache_id = 0
-        #     self.cache_page_num = 0
-        #     self.cache_file_path = os.path.join(self.cache_dir, f'raw_page_{self.cache_id}.jsonl')
-        #     self.cache_file = open(self.cache_file_path, 'w')
-        # elif self.cache_page_num >= self.pages_per_batch:
-        #     self.cache_id += 1
-        #     self.cache_page_num = 0
-        #     self.cache_file.close()
-        #     self.cache_file_path = os.path.join(self.cache_dir, f'raw_page_{self.cache_id}.jsonl')
-        #     self.cache_file = open(self.cache_file_path, 'w')
-        # self.cache_file.write(json.dumps({'page': raw_page}) + '\n')
-    
-    def _write_to_disk(self, batch_id, batch):
-        cache_file_path = os.path.join(self.cache_dir, f'raw_page_{batch_id}_not_finish.jsonl')
-        with open(cache_file_path, 'w') as cache_file:
-            for page in batch:
-                cache_file.write(json.dumps({'page': page}) + '\n')
-        os.rename(cache_file_path, os.path.join(self.cache_dir, f'raw_page_{batch_id}.jsonl'))
-    
-    def _read_file(self, input_file_path):
-        total_file_size = os.path.getsize(input_file_path)
-        parser = XMLPullParser(events=('start', 'end'))
-        raw_page = None
-        end_page = False
-        page_num = 0
-        current_batch = []
-        all_batches = []
-        update_len = 0
-        
-        # Load all batches
-        with open(input_file_path, 'r') as input_file:
-            with tqdm(total=total_file_size, desc="Processing", unit="B", unit_scale=True) as pbar:
-                for line in input_file:
-                    parser.feed(line)
-                    for event, element in parser.read_events():
-                        if event == 'start' and 'page' in element.tag:
-                            raw_page = []
-                        if event == 'end' and 'page' in element.tag:
-                            end_page = True
-                    # add line
-                    if isinstance(raw_page, list):
-                        raw_page.append(line)
-                    if end_page:
-                        raw_page = ''.join(raw_page)
-                        current_batch.append(raw_page)
-                        if len(current_batch) > self.pages_per_batch:
-                            all_batches.append(current_batch)
-                            current_batch = []
-                        page_num += 1
-                        raw_page = None
-                        end_page = False
-                    if update_len > 1e7:
-                        pbar.set_postfix_str(f"page {page_num}")
-                        pbar.update(update_len)
-                        update_len = 0
-                    else:
-                        update_len += len(line)
-        # If there are remaining pages in the last batch, add them
-        if current_batch:
-            all_batches.append(current_batch)
-
-        # Write to disk using a ThreadPool for parallel processing of batches
-        with ThreadPool(self.worker) as pool:
-            # Wrap the pool.imap with tqdm to display a progress bar
-            for batch_id, _ in tqdm(enumerate(pool.imap(
-                lambda batch: self._write_to_disk(batch_id, batch), all_batches)), 
-                total=len(all_batches), desc='Processing Batches'
-            ):
-                pass  # Just process the batch without collecting results
-            
+        return documents, no_abstract_num, print_str
     
     def remove_duplicate_documents(self, documents):
         unique_document = []
@@ -281,30 +223,56 @@ class WikipediaDumpReader():
             except Exception as e:
                 print(e)
         return english_documents
-
-    # def load_data(self) -> List[Document]:
-    #     file_documents = []
-    #     self.cache_file = None
-    #     # for filename in os.listdir(self.input_dir):
-    #     #     if filename.endswith('.xml'):
-    #     #         file_documents.extend(self._read_file(os.path.join(self.input_dir, filename)))
-    #     for filename in os.listdir(self.input_dir):
-    #         if filename.endswith('.xml'):
-    #             self._read_file(os.path.join(self.input_dir, filename))
-        
-    #     # unique_documents = self.remove_duplicate_documents(file_documents)
-    #     # english_documents = self.remove_non_english_documents(unique_documents)
-    #     if self.cache_file is not None:
-    #         self.cache_file.close()
-    #     # return unique_documents
-        
-    def parse_files(self):
-        for filename in os.listdir(self.input_dir):
-            if filename.endswith('.xml'):
-                self._read_file(os.path.join(self.input_dir, filename))
-            
+    
     def load_data(self):
-        self.parse_files()
+        if not self._processed_xml():
+            self._parse_files()
+        filenames = [filename for filename in os.listdir(self.cache_dir) if 'raw_page' in filename]
+        filenames.sort(key=lambda x: int(x.split('.')[0].split('_')[-1]))
+        
+        all_batches = []
+        documents = []
+        no_abstract_num = 0
+        
+        with tqdm(total=len(filenames), desc="process file") as pbar:
+            for filename in filenames:
+                current_batch = []
+                with open(os.path.join(self.cache_dir, filename), 'r') as cache_file:
+                    for line in cache_file:
+                        data = json.loads(line)
+                        current_batch.append(data['page'])
+                batch_id = int(filename.split('.')[0].split('_')[-1])
+                all_batches.append([batch_id, current_batch])
+                if len(all_batches) >= 10:
+                    with ThreadPool(self.worker) as pool:
+                        for batch_documents, batch_no_abstract_num, print_str in pool.imap(
+                            lambda x: self._process_batch(x[0], x[1]), all_batches
+                        ):
+                            print(print_str)
+                            documents.extend(batch_documents)
+                            no_abstract_num += batch_no_abstract_num
+                    
+                    pbar.set_postfix_str(
+                        f"{no_abstract_num}/{len(documents)}  {(no_abstract_num/len(documents))*100:.2f}%"
+                    )
+                    
+                    all_batches = []
+        
+        if current_batch:
+            all_batches.append([batch_id, current_batch])
+            
+        if len(all_batches) > 0:
+            with ThreadPool(self.worker) as pool:
+                for batch_documents, batch_no_abstract_num, print_str in pool.imap(
+                    lambda x: self._process_batch(x[0], x[1]), all_batches
+                ):
+                    print(print_str)
+                    documents.extend(batch_documents)
+                    no_abstract_num += batch_no_abstract_num
+                
+                pbar.set_postfix_str(
+                    f"{batch_no_abstract_num}/{len(documents)}  {(batch_no_abstract_num/len(documents))*100:.2f}%"
+                )
 
 if __name__ == '__main__':
     root_path = '../../..'
